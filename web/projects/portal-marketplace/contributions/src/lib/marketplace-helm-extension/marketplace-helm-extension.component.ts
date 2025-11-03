a9674783b2965742baafd97ff75838aa255bf719
Mohammad Asghari <mohammad.asghari@huma.com>
2025-11-03 10:13:41 +0000

    chore: fixed translation issue

--- web/projects/portal-marketplace/contributions/src/lib/marketplace-helm-extension/marketplace-helm-extension.component.ts
+++ web/projects/portal-marketplace/contributions/src/lib/marketplace-helm-extension/marketplace-helm-extension.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatTab, MatTabGroup, MatTabsModule } from '@angular/material/tabs';
-import { TranslationDirective } from '@huma-engineering/tool-kit/i18n';
+import {
+  provideTranslationScope,
+  TranslationDirective,
+} from '@huma-engineering/tool-kit/i18n';
+import { languageLoader } from '../i18n/src/loader';
 import { MarketplaceService } from '../service';
 import { MarketplaceItemsListComponent } from './marketplace-items-list/marketplace-items-list.component';
 
@@ -16,6 +20,7 @@ import { MarketplaceItemsListComponent } from './marketplace-items-list/marketpl
     MatTab,
     MarketplaceItemsListComponent,
   ],
+  providers: [provideTranslationScope('marketplace', languageLoader())],
   templateUrl: './marketplace-helm-extension.component.html',
   styleUrl: './marketplace-helm-extension.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
